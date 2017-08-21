b930fe6bfba944db89fd217797391a6c3ba3aa29
QING ZHU <qzhu@lbl.gov>
2017-08-21 16:38:32 -0700

    (1) AD-RG transit

--- components/clm/src/biogeochem/PhosphorusStateType.F90
+++ components/clm/src/biogeochem/PhosphorusStateType.F90
@@ -1246,19 +1246,21 @@ contains
                    ! solve equilibrium between loosely adsorbed and solution
                    ! phosphorus
                    if ((nu_com .eq. 'ECA') .or. (nu_com .eq. 'MIC')) then
-                      a = 1;
+                      a = 1
                       b = VMAX_MINSURF_P_vr(j,cnstate_vars%isoilorder(c)) + &
-                          KM_MINSURF_P_vr(j,cnstate_vars%isoilorder(c)) - cnstate_vars%labp_col(c)/zisoi(nlevdecomp);
-                      d = -1.0* cnstate_vars%labp_col(c)/zisoi(nlevdecomp) * KM_MINSURF_P_vr(j,cnstate_vars%isoilorder(c));
+                          KM_MINSURF_P_vr(j,cnstate_vars%isoilorder(c)) - cnstate_vars%labp_col(c)/zisoi(nlevdecomp-1)
+                      d = -1.0* cnstate_vars%labp_col(c)/zisoi(nlevdecomp-1) * KM_MINSURF_P_vr(j,cnstate_vars%isoilorder(c))
                    else if (nu_com .eq. 'RD') then
-                      a = 1;
+                      a = 1
                       b = smax(cnstate_vars%isoilorder(c)) + &
-                          ks_sorption(cnstate_vars%isoilorder(c)) - cnstate_vars%labp_col(c)/zisoi(nlevdecomp);
-                      d = -1.0* cnstate_vars%labp_col(c)/zisoi(nlevdecomp) - this%solutionp_vr_col(c,j)
+                          ks_sorption(cnstate_vars%isoilorder(c)) - cnstate_vars%labp_col(c)/zisoi(nlevdecomp-1)
+                      d = -1.0* cnstate_vars%labp_col(c)/zisoi(nlevdecomp-1) * ks_sorption(cnstate_vars%isoilorder(c))
                    end if
-                   this%secondp_vr_col(c,j) = cnstate_vars%secp_col(c)/zisoi(nlevdecomp)
-                   this%occlp_vr_col(c,j) = cnstate_vars%occp_col(c)/zisoi(nlevdecomp)
-                   this%primp_vr_col(c,j) = cnstate_vars%prip_col(c)/zisoi(nlevdecomp)
+                   this%solutionp_vr_col(c,j) = (-b+(b**2-4*a*d)**0.5)/(2*a)
+                   this%labilep_vr_col(c,j) = cnstate_vars%labp_col(c)/zisoi(nlevdecomp-1) - this%solutionp_vr_col(c,j)
+                   this%secondp_vr_col(c,j) = cnstate_vars%secp_col(c)/zisoi(nlevdecomp-1)
+                   this%occlp_vr_col(c,j) = cnstate_vars%occp_col(c)/zisoi(nlevdecomp-1)
+                   this%primp_vr_col(c,j) = cnstate_vars%prip_col(c)/zisoi(nlevdecomp-1)
                 end do
              else
                 this%solutionp_vr_col(c,1) = cnstate_vars%labp_col(c)/2._r8
