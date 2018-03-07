6605dc8ad0fbb9fa1af70777506c794ae68ccea3
Gautam Bisht <gbisht@lbl.gov>
2018-03-07 14:44:27 -0800

    Removes land use and product pool/fluxes from BGC summary diagnostics
    - Removes product pools from diagnostics of total ecosystem nutrients and
      total soil column nutrients.
    - Removes accounting of product loss fluxes from CNP balance check.
    - Updates CNP balance check to includes fluxes of CNP associated with:
      - Loss for two product pools
      - Harvest for two product pools

--- components/clm/src/biogeochem/PhosphorusStateType.F90
+++ components/clm/src/biogeochem/PhosphorusStateType.F90
@@ -588,12 +588,12 @@ contains
 
     this%totecosysp_col(begc:endc) = spval
     call hist_addfld1d (fname='TOTECOSYSP', units='gP/m^2', &
-         avgflag='A', long_name='total ecosystem P', &
+         avgflag='A', long_name='total ecosystem P but excl product pools', &
          ptr_col=this%totecosysp_col)
 
     this%totcolp_col(begc:endc) = spval
     call hist_addfld1d (fname='TOTCOLP', units='gP/m^2', &
-         avgflag='A', long_name='total column-level P', &
+         avgflag='A', long_name='total column-level P but excl product pools', &
          ptr_col=this%totcolp_col)
 
     this%seedp_col(begc:endc) = spval
@@ -1763,7 +1763,6 @@ contains
            this%secondp_col(c) + &
            this%primp_col(c) + &
            this%occlp_col(c) + &
-           this%totprodp_col(c) + &
            this%totvegp_col(c)
 
       ! total column phosphorus, including pft (TOTCOLP)
@@ -1775,7 +1774,6 @@ contains
            this%solutionp_col(c) + &
            this%labilep_col(c) + &
            this%secondp_col(c) + &
-           this%totprodp_col(c) + &
            this%seedp_col(c)    + &
            this%ptrunc_col(c)
    end do
