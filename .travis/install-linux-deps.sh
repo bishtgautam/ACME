539ecb83bf05c27a059044f928a194aa988271a1
Gautam Bisht <gbisht@lbl.gov>
2018-04-24 14:43:59 -0700

    minor modifications to Travis-CI script

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -3,4 +3,24 @@ sudo apt-get update -qq
 #sudo apt-get install -y cmake gcc gfortran g++ liblapack-dev libopenmpi-dev openmpi-bin
 sudo apt-get install -y cmake gcc gfortran g++
 sudo apt-get install -y netcdf-bin libnetcdf-dev
+sudo apt-get install -y libopenmpi-dev openmpi-bin
+sudo apt-get install -y libxml2 libxml2-dev libxml2-utils libxml-perl xml-core XML-LibXML
 
+which gcc
+which g++
+which gfortran
+which ncdump
+ls /usr/bin/mpi*
+
+cd cime/scripts
+
+./create_newcase --case f19_g16.ICLM45 --res f19_g16 --compset ICLM45 --mach linux-generic
+
+cd f19_g16.ICLM45
+./xmlchange DATM_CLMNCEP_YR_END=1972
+./xmlchange PIO_TYPENAME=netcdf
+./xmlchange RUNDIR=${PWD}/run
+./xmlchange EXEROOT=${PWD}/bld
+./xmlchange NTASKS=1
+./xmlchange DIN_LOC_ROOT=$PWD
+./case.setup
--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -5,10 +5,29 @@ brew upgrade cmake
 brew tap homebrew/science
 brew unlink gcc
 brew install gcc
-#brew install open-mpi
+brew install open-mpi
 brew install netcdf
 
 # Make sure the weird gfortran library links are in place.
 ln -s /usr/local/lib/gcc/5/libgfortran.dylib /usr/local/lib/libgfortran.dylib
 ln -s /usr/local/lib/gcc/5/libgfortran.a /usr/local/lib/libgfortran.a
 
+which gcc
+which g++
+which ncdump
+
+ls -l /usr/local/bin/mpi*
+
+cd cime/scripts
+./create_newcase --case f19_g16.ICLM45 --res f19_g16 --compset ICLM45 --mach linux-generic
+
+cd f19_g16.ICLM45
+./xmlchange DATM_CLMNCEP_YR_END=1972
+./xmlchange PIO_TYPENAME=netcdf
+./xmlchange RUNDIR=${PWD}/run
+./xmlchange EXEROOT=${PWD}/bld
+./xmlchange NTASKS=1
+./xmlchange DIN_LOC_ROOT=$PWD
+./case.setup
+cat Macros.make
+cat env_mach_specific.xml
