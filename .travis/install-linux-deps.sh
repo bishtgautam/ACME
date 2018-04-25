7f38fea550a52650e21230af39d3b7899e614f87
Gautam Bisht <gbisht@lbl.gov>
2018-04-25 08:16:16 -0700

    more changes for linux bot

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 #sudo apt-get install -y cmake gcc gfortran g++ liblapack-dev libopenmpi-dev openmpi-bin
-sudo apt-get install -y cmake gcc gfortran g++
+sudo apt-get install -y cmake gcc-5 gfortran-5 g++-5
 sudo apt-get install -y netcdf-bin libnetcdf-dev
 sudo apt-get install -y libopenmpi-dev openmpi-bin
 sudo apt-get install -y libxml2 libxml2-dev libxml2-utils libxml-perl xml-core gnulib
