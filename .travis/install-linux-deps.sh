de018da07de68e9a2f2ce7657bafd86b645a4769
Gautam Bisht <gbisht@lbl.gov>
2018-04-25 15:23:16 -0700

    more changes for linux bot

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,9 +1,10 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
 sudo apt-get install -y cmake gcc-5 gfortran-5 g++-5 liblapack-dev
+sudo apt-get install -y netcdf-bin libnetcdf-dev
+sudo apt-get install -y libxml2 libxml2-dev libxml2-utils libxml-perl xml-core gnulib
 
 #sudo apt-get install -y libmpich-dev mpich
-#sudo apt-get install -y libxml2 libxml2-dev libxml2-utils libxml-perl xml-core gnulib
 
 #sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 #sudo apt-get update -qq
---
+++ .travis/install-petsc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+git clone https://bitbucket.org/petsc/petsc.git
+
+PETSC_GIT_HASH=v3.8.3
+
+cd petsc
+
+git checkout ${PETSC_GIT_HASH}
+
+export PETSC_DIR=$PWD
+
+./configure PETSC_ARCH=petsc-arch \
+--with-cc=$CC \
+--with-cxx=$CXX \
+--with-fc=$FC \
+--CFLAGS='-g -O0' --CXXFLAGS='-g -O0' --FFLAGS='-g -O0 -Wno-unused-function' \
+--with-clanguage=c \
+--with-debug=$DEBUG  \
+--with-shared-libraries=0 \
+--download-hdf5 \
+--download-metis \
+--download-parmetis \
+--download-fblaslapack \
+--download-mpich=http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+
+
+make all
+make test
+
