a12c098bbef3508863fa384d1f0370ed7ffe46b4
Gautam Bisht <gbisht@lbl.gov>
2018-04-25 14:32:11 -0700

    more changes for linux bot

--- .travis/install-linux-deps.sh
+++ .travis/install-linux-deps.sh
@@ -1,9 +1,16 @@
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update -qq
+#sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+#sudo apt-get update -qq
 #sudo apt-get install -y cmake gcc gfortran g++ liblapack-dev libopenmpi-dev openmpi-bin
 #sudo apt-get install -y cmake gcc-5 gfortran-5 g++-5
 #sudo apt-get install -y libopenmpi-dev openmpi-bin
-sudo apt-get install -y cmake liblapack-dev
+#sudo apt-get install -y cmake liblapack-dev
+#sudo apt-get install -y netcdf-bin libnetcdf-dev
+#sudo apt-get install -y libmpich-dev mpich
+#sudo apt-get install -y libxml2 libxml2-dev libxml2-utils libxml-perl xml-core gnulib
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+sudo apt-get install -y cmake gcc-5 gfortran-5 g++-5 liblapack-dev
 sudo apt-get install -y netcdf-bin libnetcdf-dev
 sudo apt-get install -y libmpich-dev mpich
 sudo apt-get install -y libxml2 libxml2-dev libxml2-utils libxml-perl xml-core gnulib
@@ -17,20 +24,20 @@ which ncdump
 ls -l /usr/bin/mpi*
 ls -l /usr/bin/g*
 gcc   --version
-gcc-6 --version
-cd cime/scripts
 
-./create_newcase --case f19_g16.ICLM45 --res f19_g16 --compset ICLM45 --mach travis-ci-linux --compiler gnu
-
-cd f19_g16.ICLM45
-./xmlchange DATM_CLMNCEP_YR_END=1972
-./xmlchange PIO_TYPENAME=netcdf
-./xmlchange RUNDIR=${PWD}/run
-./xmlchange EXEROOT=${PWD}/bld
-./xmlchange NTASKS=1
-./xmlchange DIN_LOC_ROOT=$PWD
-./case.setup
-cat Macros.make
-./case.build
-cat bld/gptl.bldlog.*
-cat bld/lnd.bldlog*
+#cd cime/scripts
+#
+#./create_newcase --case f19_g16.ICLM45 --res f19_g16 --compset ICLM45 --mach travis-ci-linux --compiler gnu
+#
+#cd f19_g16.ICLM45
+#./xmlchange DATM_CLMNCEP_YR_END=1972
+#./xmlchange PIO_TYPENAME=netcdf
+#./xmlchange RUNDIR=${PWD}/run
+#./xmlchange EXEROOT=${PWD}/bld
+#./xmlchange NTASKS=1
+#./xmlchange DIN_LOC_ROOT=$PWD
+#./case.setup
+#cat Macros.make
+#./case.build
+#cat bld/gptl.bldlog.*
+#cat bld/lnd.bldlog*
