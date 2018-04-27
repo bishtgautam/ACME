fddff97bb3c3e943ef9abd5b65681c81d6361473
Gautam Bisht <gbisht@lbl.gov>
2018-04-26 20:32:21 -0700

    more changes for linux bot

--- .travis/install-osx-deps.sh
+++ .travis/install-osx-deps.sh
@@ -27,3 +27,20 @@ nc-config --all
 
 nc-config --flibs
 
+cd cime/scripts
+./create_newcase --case f19_g16.ICLM45 --res f19_g16 --compset ICLM45 --mach travis-ci-osx --compiler gnu
+
+cd f19_g16.ICLM45
+./xmlchange DATM_CLMNCEP_YR_END=1972
+./xmlchange PIO_TYPENAME=netcdf
+./xmlchange RUNDIR=${PWD}/run
+./xmlchange EXEROOT=${PWD}/bld
+./xmlchange NTASKS=1
+./xmlchange DIN_LOC_ROOT=$PWD
+./case.setup
+alias gmake=make
+cat Macros.make
+cat env_mach_specific.xml
+./case.build
+cat bld/e3sm.bldlog.*
+
